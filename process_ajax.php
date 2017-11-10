061168f3424ce0fc856cd20c15550d22120acbe3
dastangoo <hamed.dastangoo@gmail.com>
2017-11-10 10:35:22 +0330

    URL Variables and AJAX

--- process_ajax.php
+++ process_ajax.php
@@ -9,11 +9,24 @@ $names[] = 'Mark';
 $names[] = 'Shaun';
 $names[] = 'Hillary';
 $names[] = 'Walton';
-$names[] = 'Walton';
 $names[] = 'Lara';
 $c = 1;
 foreach ($names as $name) {
-  echo $c . ' ' . $name . '<br>';
-  $c++;
+  if ($_REQUEST['var1'] == $name) {
+      echo $c . ' ' . $name . ' is the important Name<br><br>';
+  } else {
+    echo $c. ' ' . $name . '<br><br>';
+  }
 }
+
+if (isset($_REQUEST['var2'])) {
+  if ($_REQUEST['var2'] == '') {
+    echo 'We have an empty variable, so we&apos;re unable to show you the result';    
+  } else {
+    echo 'We have some ' . $_REQUEST['var2'] . '. We will eat them.';
+  }
+} else {
+  echo "Note: Something is more there but is not visible because of a variable inside it, which is actually not declared any where.";
+}
+
 ?>
