167e42e63f76f7b033506cc35862e90c9067c281
dastangoo <hamed.dastangoo@gmail.com>
2017-11-10 10:17:40 +0330

    Retreiving An Array

--- process_ajax.php
+++ process_ajax.php
@@ -1,1 +1,19 @@
-<?php echo 'This is the PHP page.' ?>
+<?php 
+echo 'This is the PHP page.';
+echo '<br>';
+echo '<br>';
+print '<b>The Second Line</b>';
+print '<br>';
+
+$names[] = 'Mark';
+$names[] = 'Shaun';
+$names[] = 'Hillary';
+$names[] = 'Walton';
+$names[] = 'Walton';
+$names[] = 'Lara';
+$c = 1;
+foreach ($names as $name) {
+  echo $c . ' ' . $name . '<br>';
+  $c++;
+}
+?>
