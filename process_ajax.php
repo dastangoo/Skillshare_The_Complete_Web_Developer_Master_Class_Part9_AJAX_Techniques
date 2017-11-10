2d3df87bd35d139b52189d8756829b1337976149
dastangoo <hamed.dastangoo@gmail.com>
2017-11-10 14:53:47 +0330

    Getting Dynamic Results in the Bootstrap Modal

--- process_ajax.php
+++ process_ajax.php
@@ -26,7 +26,43 @@
           <td>$rows[u_number]</td>
           <td>$rows[u_notes]</td>
           <td>
-            <button class='btn btn-sucess'>Edit</button>
+            <button class='btn btn-sucess' data-toggle='modal' data-target='#edit_person' data-backdrop='static'>Edit</button>
+            <div class='modal fade' id='edit_person$c'>
+              <div class='modal-dialog'>
+                <div class='modal-content'>
+                  <div class='modal-header'>
+                    <button class='close' data-dismiss='modal'>&times;</button>
+                  </div>
+                  <div class='modal-body'>
+                  <form>
+                    <div class='form-group'>
+                      <label for='name'>Name</label>
+                      <input type='text' class='form-control' id='name' value=$rows[u_name]>
+                    </div>
+                    <div class=form-group>
+                      <label for='email'>Email</label>
+                      <input type='text' class='form-control' id='email'>
+                    </div>
+                    <div class='form-group'>
+                      <label for='contact_number'></label>
+                      <input type='text' class='form-control' id='contact_number'>
+                    </div>
+                    <div class='form-group'>
+                      <label for='notes'></label>
+                      <textarea class='form-control' id='notes'  rows='5'></textarea>
+                    </div>
+                    <div class='form-group'>
+                      <button class='btn btn-info btn-block btn-lg' id='submit'>Done Editing</button>
+                    </div>                  
+                  </form>
+
+                  </div>
+                  <div class='modal-footer'>
+                    <button class='btn btn-danger' data-dismiss='modal'>Close</button>
+                  </div>
+                </div>
+              </div>
+            </div>
             <button class='btn btn-danger' onclick=delete_func('$rows[user_id]');>Delete</button>
           </td>
         </tr>
