a5851f62d2d3444ce16874db3dfe5749250abc68
dastangoo <hamed.dastangoo@gmail.com>
2017-11-10 11:01:45 +0330

    Input and Output

--- process_ajax.php
+++ process_ajax.php
@@ -1,32 +1,19 @@
 <?php 
-echo 'This is the PHP page.';
-echo '<br>';
-echo '<br>';
-print '<b>The Second Line</b>';
-print '<br>';
-
-$names[] = 'Mark';
-$names[] = 'Shaun';
-$names[] = 'Hillary';
-$names[] = 'Walton';
-$names[] = 'Lara';
-$c = 1;
-foreach ($names as $name) {
-  if ($_REQUEST['var1'] == $name) {
-      echo $c . ' ' . $name . ' is the important Name<br><br>';
-  } else {
-    echo $c. ' ' . $name . '<br><br>';
+  $countries[] = 'United States';
+  $countries[] = 'United Kingdom';
+  $countries[] = 'United Arab Emirates';
+  $countries[] = 'Brazil';
+  $countries[] = 'India';
+  $countries[] = 'Pakistan';
+  $countries[] = 'Srilanka';
+  
+  echo '<br>';
+  foreach ($countries as $country) {
+    // echo $country;
+    if (isset($_REQUEST['var1'])) {
+      if($_REQUEST['var1'] == $country) {
+        echo '<div style="color: green;">' . $_REQUEST['var1'] . ' is in the list</div>';
+      }
+    }
   }
-}
-
-if (isset($_REQUEST['var2'])) {
-  if ($_REQUEST['var2'] == '') {
-    echo 'We have an empty variable, so we&apos;re unable to show you the result';    
-  } else {
-    echo 'We have some ' . $_REQUEST['var2'] . '. We will eat them.';
-  }
-} else {
-  echo "Note: Something is more there but is not visible because of a variable inside it, which is actually not declared any where.";
-}
-
 ?>
